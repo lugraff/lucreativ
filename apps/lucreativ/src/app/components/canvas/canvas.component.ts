fd4090b360fd572abde8d7ba1be31e0394636c76
Lugraff <lugraff@gmx.net>
2023-02-11 23:11:55 +0100

    dotknot remake

--- apps/lucreativ/src/app/components/canvas/canvas.component.ts
+++ apps/lucreativ/src/app/components/canvas/canvas.component.ts
@@ -1,7 +1,9 @@
 import { AfterViewInit, Component, NgZone, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Vector2 } from '@shared/util-global';
+import { MouseEventService, Vector2 } from '@shared/util-global';
 import { IsMobileScreenService } from '@shared/util-screen';
+import { FormsModule } from '@angular/forms';
+import { ButtonStandardComponent } from '@shared/ui-global';
 
 export interface Ball {
   pos: Vector2;
@@ -13,25 +15,28 @@ export interface Ball {
 @Component({
   selector: 'lucreativ-bugs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ButtonStandardComponent],
   templateUrl: './canvas.component.html',
 })
 export class CanvasComponent implements AfterViewInit, OnDestroy {
   private canvas: HTMLCanvasElement | undefined = undefined;
   private processing = false;
+  public connectDist = 140;
+  public dots = 100;
+  public minSpeed = 0;
+  public maxSpeed = 2;
+  private mousePos: Vector2 = { x: 0, y: 0 };
 
-  private balls: Ball[] = [
-    // { pos: { x: 50, y: 50 }, dir: { x: 1, y: 1 }, speed: 5 },
-    // { pos: { x: 10, y: 150 }, dir: { x: -1, y: 1 }, speed: 4 },
-  ];
+  private balls: Ball[] = [];
 
-  constructor(private ngZone: NgZone, public screenService: IsMobileScreenService) {
-    for (let index = 0; index < 100; index++) {
+  constructor(private ngZone: NgZone, public screenService: IsMobileScreenService, mouseService: MouseEventService) {
+    mouseService.mouseMove.subscribe((event) => (this.mousePos = { x: event.clientX, y: event.clientY }));
+    for (let index = 0; index < this.dots; index++) {
       const newBall: Ball = {
         pos: { x: Math.random() * window.innerWidth, y: Math.random() * window.innerHeight },
-        dir: { x: Math.random() - 0.5, y: Math.random() - 0.5 },
-        speed: Math.random() + 0.1,
-        radius: Math.random() * 50 + 1,
+        dir: { x: (Math.random() - 0.5) * 2, y: (Math.random() - 0.5) * 2 },
+        speed: Math.random() * this.maxSpeed,
+        radius: Math.random() * 2 + 1,
       };
       this.balls.push(newBall);
     }
@@ -52,46 +57,99 @@ export class CanvasComponent implements AfterViewInit, OnDestroy {
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
     for (const ball of this.balls) {
-      this.calcBallPos(ball);
+      this.calcMouseGravity(ball);
     }
     for (const ball of this.balls) {
-      this.paintBall(ctx, ball);
+      this.calcBallPos(ball);
     }
-
-    ctx.shadowBlur = 4;
-    ctx.shadowOffsetX = 6;
-    ctx.shadowColor = 'black';
-
+    // for (const ball of this.balls) {
+    //   this.paintBall(ctx, ball);
+    // }
+    for (let index = 0; index < this.balls.length; index++) {
+      this.paintLine(ctx, index);
+    }
     requestAnimationFrame(() => this.process());
+  }
+
+  private calcMouseGravity(ball: Ball): void {
+    const distance = Math.sqrt(
+      (ball.pos.x - this.mousePos.x) * (ball.pos.x - this.mousePos.x) +
+        (ball.pos.y - this.mousePos.y) * (ball.pos.y - this.mousePos.y)
+    );
+    if (distance < this.connectDist * 2) {
+      ball.speed += distance * 0.001;
+      if (ball.speed > this.maxSpeed) {
+        ball.speed = this.maxSpeed;
+      }
+    } else {
+      if (ball.speed > this.minSpeed) {
+        ball.speed -= distance * 0.00005;
+      } else {
+        ball.speed = this.minSpeed;
+      }
+    }
   }
 
   private calcBallPos(ball: Ball): void {
     ball.pos.x += ball.dir.x * ball.speed;
     ball.pos.y += ball.dir.y * ball.speed;
-    ball.speed += Math.random() * 0.1;
-    if (ball.speed < 0) {
-      ball.speed = 1;
-    } else if (ball.speed > 1) {
-      ball.speed = 1;
-    }
-
     if (ball.pos.y < ball.radius) {
-      ball.dir.y = 1;
+      ball.pos.y = ball.radius;
+      ball.dir.y = -ball.dir.y;
     } else if (ball.pos.y + ball.radius > window.innerHeight) {
-      ball.dir.y = -1;
+      ball.pos.y = window.innerHeight - ball.radius;
+      ball.dir.y = -ball.dir.y;
     }
     if (ball.pos.x < ball.radius) {
-      ball.dir.x = 1;
+      ball.pos.x = ball.radius;
+      ball.dir.x = -ball.dir.x;
     } else if (ball.pos.x + ball.radius > window.innerWidth) {
-      ball.dir.x = -1;
+      ball.pos.x = window.innerWidth - ball.radius;
+      ball.dir.x = -ball.dir.x;
     }
   }
 
   private paintBall(ctx: CanvasRenderingContext2D, ball: Ball): void {
-    ctx.fillStyle = '#A3C832aa';
+    ctx.fillStyle = '#ffffffaa';
     const circle = new Path2D();
     circle.arc(ball.pos.x, ball.pos.y, ball.radius, 0, 2 * Math.PI);
     ctx.fill(circle);
+  }
+
+  private paintLine(ctx: CanvasRenderingContext2D, ballIndex: number): void {
+    const posSelf: Vector2 = this.balls[ballIndex].pos;
+
+    for (let index = ballIndex; index < this.balls.length; index++) {
+      const posOther: Vector2 = this.balls[index].pos;
+      const distance = Math.sqrt(
+        (posSelf.x - posOther.x) * (posSelf.x - posOther.x) + (posSelf.y - posOther.y) * (posSelf.y - posOther.y)
+      );
+      if (distance < this.connectDist) {
+        ctx.strokeStyle = `rgba(
+          ${distance * 4},
+          255,
+          ${distance * 4},
+          ${(this.connectDist - distance) / this.connectDist})`;
+        ctx.beginPath();
+        ctx.moveTo(posSelf.x, posSelf.y);
+        ctx.lineTo(this.balls[index].pos.x, this.balls[index].pos.y);
+        ctx.closePath();
+        ctx.stroke();
+      }
+    }
+  }
+
+  onReload() {
+    this.balls = [];
+    for (let index = 0; index < this.dots; index++) {
+      const newBall: Ball = {
+        pos: { x: Math.random() * window.innerWidth, y: Math.random() * window.innerHeight },
+        dir: { x: (Math.random() - 0.5) * 2, y: (Math.random() - 0.5) * 2 },
+        speed: Math.random() * this.maxSpeed,
+        radius: Math.random() * 2 + 1,
+      };
+      this.balls.push(newBall);
+    }
   }
 
   public ngOnDestroy(): void {
