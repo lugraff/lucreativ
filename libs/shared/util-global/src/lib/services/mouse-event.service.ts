c93824d74a43e0fbcebab5b96c3d4bec42d144bd
Lugraff <lugraff@gmx.net>
2023-03-03 16:28:38 +0100

    popup touch test

--- libs/shared/util-global/src/lib/services/mouse-event.service.ts
+++ libs/shared/util-global/src/lib/services/mouse-event.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { IsMobileScreenService } from '@shared/util-screen';
 import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
@@ -10,18 +11,41 @@ export class MouseEventService {
   mouseUp = new Subject<MouseEvent>();
   mouseDown = new Subject<MouseEvent>();
 
-  constructor() {
-    window.addEventListener('mousedown', (event) => {
-      this.mouseDown.next(event);
-      this.mouseLastEvent.next(event);
-    });
-    window.addEventListener('mousemove', (event) => {
-      this.mouseMove.next(event);
-      this.mouseLastEvent.next(event);
-    });
-    window.addEventListener('mouseup', (event) => {
-      this.mouseUp.next(event);
-      this.mouseLastEvent.next(event);
-    });
+  constructor(public machineInfoService: IsMobileScreenService) {
+    if (machineInfoService.isMobile) {
+      window.addEventListener('touchstart', (event) => {
+        this.mouseDown.next(new MouseEvent('mouseDown', this.emulateEvent(event.changedTouches[0])));
+      });
+      window.addEventListener('touchmove', (event) => {
+        this.mouseDown.next(new MouseEvent('mousemove', this.emulateEvent(event.changedTouches[0])));
+      });
+      window.addEventListener('touchend', (event) => {
+        this.mouseDown.next(new MouseEvent('mouseup', this.emulateEvent(event.changedTouches[0])));
+      });
+    } else {
+      window.addEventListener('mousedown', (event) => {
+        this.mouseDown.next(event);
+        this.mouseLastEvent.next(event);
+      });
+      window.addEventListener('mousemove', (event) => {
+        this.mouseMove.next(event);
+        this.mouseLastEvent.next(event);
+      });
+      window.addEventListener('mouseup', (event) => {
+        this.mouseUp.next(event);
+        this.mouseLastEvent.next(event);
+      });
+    }
+  }
+
+  private emulateEvent(touch: Touch): MouseEventInit {
+    const emulatedEvent: MouseEventInit = {
+      clientX: touch.clientX,
+      clientY: touch.clientY,
+      relatedTarget: touch.target,
+      screenX: touch.screenX,
+      screenY: touch.screenY,
+    };
+    return emulatedEvent;
   }
 }
